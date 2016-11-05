1bc9a8df11a7762296908dd995613363703d56f9
Franklin Schrans <f.schrans@me.com>
2016-11-05 11:44:20 +0000

    Add HTML route

--- Hello/Sources/App/main.swift
+++ Hello/Sources/App/main.swift
@@ -2,8 +2,22 @@ import Vapor
 
 let drop = Droplet()
 
+drop.get("html") { request in
+  return try drop.view.make("index.html")
+}
+
 drop.get("hello") { request in
-  return "Hello, world!"
+  guard let name = request.data["name"]?.string else {
+    throw Abort.badRequest
+  }
+  
+  return "Hello \(name)"
+}
+
+drop.get("json") { request in
+  return try JSON(node: [
+    "version": "1.0"
+  ])
 }
 
 drop.run()
